3eb4adb85275bb3dc418cfe26d73a3fb32b65704
kiwiqiqi <isabella.qilu2000@gmail.com>
2018-02-24 14:25:36 -0500

    fix the eblock

--- client/main.js
+++ client/main.js
@@ -6,16 +6,11 @@ import './main.html';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
-  EthBlocks.init();
 });
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
-  },
-
-  currentBlock() {
-    return EthBlocks.latest.number;
   }
 });
 
