4b04d18696b906fa34bd74b6b38a483bec6a9c42
Zhongjie He <eric6356@gmail.com>
2018-04-14 18:45:59 -0400

    api register

--- imports/methods.js
+++ imports/methods.js
@@ -1,25 +1,36 @@
+/**
+ * Register api as meteor methods
+ */
 import { Meteor } from 'meteor/meteor'
 import { bank, auditor, public } from './api'
 
-Meteor.methods({
-  'public.getStockList' () {
-      if (!Meteor.isServer) {
-          return
-      }
-      return public.getStockList({ i: 4 })
-  },
+const methods = {}
 
-  'public.getPublicBuyOfferList' () {
-      if (!Meteor.isServer) {
-          return
-      }
-      return public.getPublicBuyOfferList({ i: 4 })
-  },
-
-  'public.getPublicSellOfferList' () {
-      if (!Meteor.isServer) {
-          return
-      }
-      return public.getPublicSellOfferList({ i: 4 })
-  }
+Object.keys(public).forEach(key => {
+    methods[`public.${key}`] = () => {
+        if (!Meteor.isServer) {
+            return
+        }
+        return public[key]({ i: 4 })
+    }
 })
+
+Object.keys(bank).forEach(key => {
+    methods[`bank.${key}`] = args => {
+        if (!Meteor.isServer) {
+            return
+        }
+        return bank[key](args)
+    }
+})
+
+Object.keys(auditor).forEach(key => {
+    methods[`auditor.${key}`] = args => {
+        if (!Meteor.isServer) {
+            return
+        }
+        return auditor[key](args)
+    }
+})
+
+Meteor.methods(methods)
